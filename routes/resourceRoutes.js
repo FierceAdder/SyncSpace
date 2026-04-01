80032444ed708141e1a5145f442514ad44e741b8
Ashmit <ashmitv704@gmail.com>
2026-04-02 02:24:21 +0530

    added read and update routes for resources

--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -38,4 +38,83 @@ router.post('/add',verifyToken,async (req,res)=>{
     }
 });
 
+router.get("/:groupId",verifyToken,async (req,res)=>{
+    try{
+        const groupId=req.params.groupId;
+        const userId=req.user.id;
+        const isMember=(await User.exists({_id : userId,Groups_Part_Of : groupId}));
+        if(isMember){
+            const resources = await Resource.find({ Group_Posted_In: groupId }).populate('Posted_By', 'UserName');
+            res.status(200).json({
+                resources
+            });
+        }else{
+            res.status(403).json({
+                "message" : "You are not a member of this group."
+            })
+        }
+    }catch(err){
+        console.log("Error : ",err);
+        res.status(500).json({
+            "message" : "Error Occured"
+        })
+    }
+});
+
+router.put("/:resourceId/upvote", verifyToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const resourceId = req.params.resourceId;
+        const resource = await Resource.findById(resourceId);
+        
+        if (!resource) {
+            return res.status(404).json({ message: "Resource Not Found." }); 
+        }
+        
+        if (resource.Upvotes.some(id => id.toString() === userId)) {
+            resource.Upvotes.pull(userId);
+            await resource.save();
+            res.status(200).json({ message: "Upvote removed!" }); 
+        } else {
+            if (resource.Downvotes.some(id => id.toString() === userId)) {
+                resource.Downvotes.pull(userId);
+            }
+            resource.Upvotes.push(userId);
+            await resource.save();
+            res.status(200).json({ message: "Upvote added!" }); 
+        }
+    } catch (err) {
+        console.log("Error : ", err);
+        res.status(500).json({ message: "Error Occured" });
+    }
+});
+
+router.put("/:resourceId/downvote", verifyToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const resourceId = req.params.resourceId;
+        const resource = await Resource.findById(resourceId);
+        
+        if (!resource) {
+            return res.status(404).json({ message: "Resource Not Found." }); 
+        }
+        
+        if (resource.Downvotes.some(id => id.toString() === userId)) {
+            resource.Downvotes.pull(userId);
+            await resource.save();
+            res.status(200).json({ message: "Downvote removed!" }); 
+        } else {
+            if (resource.Upvotes.some(id => id.toString() === userId)) {
+                resource.Upvotes.pull(userId);
+            }
+            resource.Downvotes.push(userId);
+            await resource.save();
+            res.status(200).json({ message: "Downvote added!" });
+        }
+    } catch (err) {
+        console.log("Error : ", err);
+        res.status(500).json({ message: "Error Occured" });
+    }
+});
+
 module.exports = router;
